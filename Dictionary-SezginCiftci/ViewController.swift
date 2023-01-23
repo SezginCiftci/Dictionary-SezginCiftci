34e12c28b24961c6bb086eeb593033fd69e71522
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2023-01-23 19:50:48 +0300

    Initial Commit

---
+++ Dictionary-SezginCiftci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dictionary-SezginCiftci
+//
+//  Created by Sezgin Çiftci on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
